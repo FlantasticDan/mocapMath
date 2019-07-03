319e6110d9051219d31b26900e9f81a36fa0dace
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2019-07-03 19:40:16 -0400

    Blender Track Export v1

--- blenderTrackExport.py
+++ blenderTrackExport.py
@@ -1,1 +1,43 @@
+#!/usr/bin/env python3
+
 # exports tracker positions
+
+# Configure Workspace
+import bpy
+D = bpy.data
+C = bpy.context
+
+# identify scene and tracks in blender data blocks
+SCENE = C.scene
+CLIP = D.movieclips[0]
+TRACKER = CLIP.tracking.tracks
+
+# create export file
+filepath = C.blend_data.filepath
+filepath = filepath[:-6] # strip '.blend'
+filepath = filepath.split("\\\\")
+filePath = ""
+for x in filepath:
+    filePath = filePath + x + "\\"
+EXPORT = open(filePath[:-1] + "_TRACKERexport.txt", "x")
+
+# write header to export file
+EXPORT.write("TRACKER DATA EXPORT for {}\n\n".format(CLIP.name))
+EXPORT.write("RANGE {} to {}\n\n".format(SCENE.frame_start, SCENE.frame_end))
+
+# export tracker coordinates
+cTrack = 0
+cMarker = 0
+while cTrack < len(TRACKER):
+    EXPORT.write("\n##### {}\n".format(TRACKER[cTrack].name))
+    cMarker = 0
+    while cMarker < len(TRACKER[cTrack].markers):
+        if TRACKER[cTrack].markers[cMarker].mute is not True: # check for valid track
+            EXPORT.write("{:05d} ".format(TRACKER[cTrack].markers[cMarker].frame))
+            EXPORT.write("{:6f} ".format(TRACKER[cTrack].markers[cMarker].co[0]))
+            EXPORT.write("{:6f}\n".format(TRACKER[cTrack].markers[cMarker].co[1]))
+        cMarker = cMarker + 1
+    cTrack = cTrack + 1
+
+# close export file
+EXPORT.close()
