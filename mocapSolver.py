ae07e34449ac7caf30baccffbb713cb519374445
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2019-07-04 17:35:04 -0400

    Added method to find point on projected line

--- mocapSolver.py
+++ mocapSolver.py
@@ -2,6 +2,7 @@
 
 # solves 3d motion capture points from exported camera and tracker data
 
+import math
 import tkinter as tk
 from tkinter import filedialog
 import numpy as np
@@ -33,22 +34,28 @@ def cameraRead(CAMERA_FILE):
 
     for i, line in enumerate(CAMERA_FILE):
         if i == 0:
-            CLIP.append(line[23:])
+            #CLIP.append(line[23:])
+            cameraTrack['clip'] = line[23:-1]
         elif i == 2:
             split = line[:-1].split(" ")
-            FRAME_RANGE.append((split[1], split[3]))
+            #FRAME_RANGE.append((split[1], split[3]))
+            cameraTrack['frame_range'] = (split[1], split[3])
         elif i == 4:
             split = line[:-1].split(" ")
-            RESOLUTION.append((split[1], split[3]))
+            #RESOLUTION.append((split[1], split[3]))
+            cameraTrack['resolution'] = (split[1], split[3])
         elif i == 6:
             split = line[:-1].split(" ")
-            SENSOR.append((split[1], split[3]))
+            #SENSOR.append((split[1], split[3]))
+            cameraTrack['sensor'] = (split[1], split[3])
         elif i == 8:
             split = line[:-1].split(" ")
-            LENS.append(split[1])
+            #LENS.append(split[1])
+            cameraTrack['lens'] = split[1]
         elif i == 10:
             split = line[:-1].split(" ")
-            AOV.append((split[1], split[3]))
+            #AOV.append((split[1], split[3]))
+            cameraTrack['aov'] = (split[1], split[3])
         elif i > 12:
             split = line[:-1].split(" ")
             cameraTrack[int(split[0])] = (split[1], split[2], split[3],
@@ -65,16 +72,20 @@ def trackerRead(TRACKER_FILE):
 
     for i, line in enumerate(TRACKER_FILE):
         if i == 0:
-            CLIP.append(line[24:-1])
+            #CLIP.append(line[24:-1])
+            trackTrack['clip'] = line[24:-1]
         elif i == 2:
             split = line[:-1].split(" ")
-            FRAME_RANGE.append((split[1], split[3]))
+            #FRAME_RANGE.append((split[1], split[3]))
+            trackTrack['frame_range'] = (split[1], split[3])
         elif i == 4:
             split = line[:-1].split(" ")
-            RESOLUTION.append((split[1], split[3]))
+            #RESOLUTION.append((split[1], split[3]))
+            trackTrack['resolution'] = (split[1], split[3])
         elif i == 6:
             split = line[:-1].split(" ")
-            TRACK_NUM.append(split[1])
+            #TRACK_NUM.append(split[1])
+            trackTrack['track_num'] = split[1]
         elif i > 8:
             split = line[:-1].split(" ")
             if split[0] == "#####":
@@ -93,3 +104,47 @@ A_CAM = cameraRead(C1_CAM)
 B_CAM = cameraRead(C2_CAM)
 A_TRACK = trackerRead(C1_TRACK)
 B_TRACK = trackerRead(C2_TRACK)
+
+def angleOfViewCalc(cam, aov, track):
+
+    '''Calculates the angle of view distortion on the projected
+    track and adds that to the camera rotation.  Returns tuple.'''
+
+    trackAOV = []
+    finalRotation = []
+
+    for i in range(0, 1):
+        trackAOV[i] = ((track[i] - 0.5) * aov[i])
+        finalRotation[i] = trackAOV[i] + cam[i]
+
+    finalRotation[2] = cam[2]
+
+    return finalRotation
+
+
+def pointsOnLine(cameraTransform, track, frame, marker):
+
+    '''Calculates 2 points on the line drawn between the camera origin and the
+    projected point on the track projection.  Returns as 2 numpy arrays.'''
+
+    # extract camera variables
+    originPoint = (cameraTransform[frame][0], cameraTransform[frame][1], cameraTransform[frame][2])
+    cameraRotation = (cameraTransform[frame][3], cameraTransform[frame][4],
+                      cameraTransform[frame][5])
+    cameraAOV = (cameraTransform['aov'][0], cameraTransform['aov'][1])
+
+    # extract tracker variables
+    for x in track[marker]:
+        if x[0] == frame:
+            trackPosition = (x[1], x[2])
+
+    # account for marker based angle modifers
+    rotation = angleOfViewCalc(cameraRotation, cameraAOV, trackPosition)
+
+    # calculate arbitary second point on projected line
+    newPoint = []
+    for i in range(0, 2):
+        newPoint[i] = math.cos(rotation[i]) * 10 + originPoint[i]
+
+    return (np.array([originPoint[0], originPoint[1], originPoint[2]]),
+            np.array([newPoint[0], newPoint[1], newPoint[2]]))
