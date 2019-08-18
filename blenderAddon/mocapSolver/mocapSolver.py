9902d21ad96bb631ac263720dc35ea50dd7fe919
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2019-08-18 16:41:44 -0400

    Relative Path File Management

--- blenderAddon/mocapSolver/mocapSolver.py
+++ blenderAddon/mocapSolver/mocapSolver.py
@@ -10,13 +10,23 @@ import sys
 import numpy as np
 import mathutils
 
+# File Management
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 # configure UI to hide default window
 root = tk.Tk()
 root.withdraw()
-root.iconbitmap(default=os.path.join(sys.path[0], 'redTri.ico'))
+root.iconbitmap(default=resource_path("redTri.ico"))
 
 # UI Instructions
-readme = open(os.path.join(sys.path[0], "solverReadme.txt"))
+readme = open(resource_path("solverReadme.txt"))
 for instruction in readme:
     print(instruction[:-1])
 readme.close()
@@ -341,9 +351,9 @@ for joint in JOINTS:
             else:
                 lastFrame = lineCross(markers[0], markers[1], w-1, A_CAM, B_CAM, A_TRACK, B_TRACK)
                 thisFrame = lineCross(markers[0], markers[1], w, A_CAM, B_CAM, A_TRACK, B_TRACK)
-                new_X = (thisFrame[0] - lastFrame [0]) + EXPORT[joint][w-1][0]
-                new_Y = (thisFrame[1] - lastFrame [1]) + EXPORT[joint][w-1][1]
-                new_Z = (thisFrame[2] - lastFrame [2]) + EXPORT[joint][w-1][2]
+                new_X = (thisFrame[0] - lastFrame[0]) + EXPORT[joint][w-1][0]
+                new_Y = (thisFrame[1] - lastFrame[1]) + EXPORT[joint][w-1][1]
+                new_Z = (thisFrame[2] - lastFrame[2]) + EXPORT[joint][w-1][2]
                 EXPORT[joint][w] = (new_X, new_Y, new_Z)
 
 # export coordinate data
