e7b7ed9b077b1710d6e67c124ca00d9a967adffa
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2019-11-03 01:04:36 -0500

    Tracker Logistics

--- openCV/markerTracking.py
+++ openCV/markerTracking.py
@@ -3,8 +3,6 @@
 import os
 import numpy as np
 import cv2
-from shapely.geometry.point import Point
-from shapely.geometry import LineString
 import markerDetection as detect
 
 def fixedUpdateTracking(imgSequence, interval=24):
@@ -19,19 +17,30 @@ def fixedUpdateTracking(imgSequence, interval=24):
     Returns:
         Dictionary of Marker IDs corresponding to array of centroids.
     """
-    img = 0
-    sinceInterval = 0
-    currentImg = imgSequence[img]
-    detection = detect.markerID(currentImg)
+    total = len(imgSequence) - 1
+    start = 0
+    end = interval
 
-    for color in detect.COLOR_ID:
-        for pattern in detect.PATTERN_ID:
-            marked = detection[color][pattern]
-            if marked is None:
-                continue
-            trackedBoxes = fixedUpdateTracker(imgSequence[img:img+interval], marked[1])
-            for box in trackedBoxes:
-                print(box)
+    # Create empty marker dictionary full of empty dictionaries to store output.
+    tracks = detect.createEmptyMarkerDictionary({})
+
+    # Loop over each Chunck
+    while end <= total:
+        if end > total:
+            end = total # Catch last chunck to prevent an index error.
+        detection = detect.markerID(imgSequence[start])
+        for color in detect.COLOR_ID:
+            for pattern in detect.PATTERN_ID:
+                marked = detection[color][pattern] # Detect markers for first frame of the chunck.
+                if marked is None:
+                    continue
+                trackedBoxes = fixedUpdateTracker(imgSequence[start:end], marked[1])
+                for i, box in enumerate(trackedBoxes):
+                    center = findCenterOfBounding(box)
+                    tracks[color][pattern][i + start] = center
+        start = end
+        end += interval
+    return tracks
 
 def fixedUpdateTracker(imgSequence, corners):
     """Tracks markers across an image sequence for the Fixed Updated Method."""
@@ -46,12 +55,15 @@ def fixedUpdateTracker(imgSequence, corners):
     while iteration < len(imgSequence):
         success, boundingBox = tracker.update(imgSequence[iteration])
         if success is False:
-            print('Tracker Lost')
             break
         boxes.append(boundingBox)
         iteration += 1
-    
+
     return boxes
+
+def findCenterOfBounding(bbox):
+    """Given a bounding box, return the centeroid."""
+    return (bbox[0] + (bbox[2] / 2), bbox[1] - (bbox[3] / 2))
 
 def directoryToImgSequence(directoryPath):
     """From a directory, returns a list of openCV image objects."""
@@ -72,4 +84,4 @@ def directoryToImgSequence(directoryPath):
 
 # dirPath = filedialog.askdirectory(title='Select a Directory Containing an Image Sequence')
 # images = directoryToImgSequence(dirPath)
-# fixedUpdateTracking(images)
+# print(fixedUpdateTracking(images))
